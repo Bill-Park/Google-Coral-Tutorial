223a26ded9e38a364b718e807e2cc7295ac1c1ee
Bill-Park <tkddn0916@gmail.com>
2020-02-22 15:36:32 +0000

    test contour

--- edge/edgeContour.py
+++ edge/edgeContour.py
@@ -1,25 +1,19 @@
 import cv2
+import os
 
-cap = cv2.VideoCapture(0)
+baseDir = os.getcwd()
+sampleDir = os.path.join(baseDir, os.pardir, "sample")
 
-cap.set(cv2.CAP_PROP_FRAME_WIDTH, 640)
-cap.set(cv2.CAP_PROP_FRAME_HEIGHT, 480)
+frame = cv2.imread(os.path.join(sampleDir, "test_image.png"))
+frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-while True :
-    ret, frame = cap.read()
-    frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-    _, img_binary = cv2.threshold(frame_gray, 127, 255, 0)
-    contours, hierarchy = cv2.findContours(img_binary, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
+_, img_binary = cv2.threshold(frame_gray, 170, 255, 0)
+contours, hierarchy = cv2.findContours(img_binary, cv2.RETR_LIST, cv2.CHAIN_APPROX_SIMPLE)
 
-    for cnt in contours:
-        cv2.drawContours(frame, [cnt], 0, (255, 0, 0), 3)  # blue
+for cnt in contours:
+    cv2.drawContours(frame, [cnt], 0, (255, 0, 0), 3)  # blue
 
-    cv2.imshow("VideoFrame", frame)
+cv2.imwrite("contour.png", frame)
+cv2.imwrite("test.png", img_binary)
 
-    cv2.imshow("binary", img_binary)
-
-    if cv2.waitKey(1) > 0: break
-
-cap.release()
-cv2.destroyAllWindows()
