84d7fe67ca65d960e688f08c77f75eec1ba7714d
Bill-Park <tkddn0916@gmail.com>
2020-02-23 07:18:02 +0000

    face recognition

---
+++ haar/face.py
@@ -0,0 +1,22 @@
+import cv2
+import os
+
+face_cascade = cv2.CascadeClassifier('haar/data/haarcascades/haarcascade_frontalcatface.xml')
+
+baseDir = os.getcwd()
+sampleDir = os.path.join(baseDir, os.pardir, "sample")
+haarDir = os.path.join(baseDir, os.pardir, "haar", "data", "haarcascades")
+
+faceCascade = cv2.CascadeClassifier(os.path.join(haarDir, "haarcascade_frontalface_default.xml"))
+
+frame = cv2.imread(os.path.join(sampleDir, "test_image.png"))
+frame_gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+
+faces = faceCascade.detectMultiScale(frame_gray, 1.3, 5)
+
+print(faces)
+
+for (x,y,w,h) in faces :
+    cv2.rectangle(frame, (x,y), (x+w, y+h), (255, 0, 0), 2)
+
+cv2.imwrite("face.png", frame)
